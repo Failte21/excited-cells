9339b162a37da520ee7ef19ee990a7af446532a2
= <lsimon@greenflex.com>
2020-10-14 12:38:27 +0200

    excitedCells: cleaned and optimized

--- src/excitedCells.ts
+++ src/excitedCells.ts
@@ -1,14 +1,13 @@
-const getNeighbors = (state: number[], index: number) => {
-  const len = state.length;
+const getNeighbors = (state: number[], index: number, len: number) => {
   if (index > 0 && index < len - 1) return [state[index - 1], state[index + 1]];
   else if (index === 0) return [state[len - 1], state[index + 1]];
   else return [state[index - 1], state[0]];
 };
 
-const updateState = (state: number[]) => {
-  const newState = [...Array(state.length).map((_) => 0)];
+const updateState = (state: number[], len: number) => {
+  const newState = [...Array(len).map((_) => 0)];
   state.forEach((_, index) => {
-    const [left, right] = getNeighbors(state, index);
+    const [left, right] = getNeighbors(state, index, len);
     if (left === 1 && right === 1) newState[index] = 0;
     else if (left === 1 || right === 1) newState[index] = 1;
     else newState[index] = 0;
@@ -17,9 +16,9 @@ const updateState = (state: number[]) => {
 };
 
 const excitedCells = (initialState: number[], nSteps: number) => {
-  const finalState = [...Array(nSteps).keys()].reduce((acc, index) => {
-    const updatedState = updateState(acc);
-    console.log(updatedState);
+  const len = initialState.length;
+  const finalState = [...Array(nSteps)].reduce((acc) => {
+    const updatedState = updateState(acc, len);
     return updatedState;
   }, initialState);
   return finalState;
